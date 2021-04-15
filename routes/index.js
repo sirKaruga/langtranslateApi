78106253571c7bb649a79525da332122321dcf34
Dennis <tonnydennis443@gmail.com>
2021-04-16 00:08:37 +0300

    make it even the best

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send({ name: "Dennis", age: 26, likes: "coding" });
+  res.send({
+    name: "Dennis",
+    age: 26,
+    likes: "coding",
+    simpleMessage: "this is awesome hosting",
+  });
 });
 
 module.exports = router;
