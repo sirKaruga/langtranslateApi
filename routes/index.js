312bcbfaa923232d9fb596e9b1d69473884b7ce9
Dennis <tonnydennis443@gmail.com>
2021-10-03 08:05:54 +0300

    jh

--- routes/index.js
+++ routes/index.js
@@ -104,24 +104,6 @@ router.post("/record", async function (req, res, next) {
 });
 
 ///get item
-router.get("/", async function (req, res, next) {
-  
-  var req = unirest("GET", "https://sandbox.safaricom.co.ke/oauth/v1/generate");
-
-  req.query({
-    grant_type: "client_credentials",
-  });
-
-  req.headers({
-    Authorization:
-      "Basic SWZPREdqdkdYM0FjWkFTcTdSa1RWZ2FTSklNY001RGQ6WUp4ZVcxMTZaV0dGNFIzaA==",
-  });
-
-  req.end((res) => {
-    if (res.error) throw new Error(res.error);
-    console.log(res.body);
-  });
-});
 
 router.get("/record", async function (req, res, next) {
   var dbVitals = await myDb();
